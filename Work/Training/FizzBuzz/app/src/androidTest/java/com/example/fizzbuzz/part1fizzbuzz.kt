5cbc5362072ba9ebd0eca898135ac1a258f7c975
Tasneem Kurabadwala <Tasneem.Kurabadwala@softwire.com>
2024-06-25 11:44:14 +0100

    Main kotlin file has been updated with code to include Part 2 rules

--- Work/Training/FizzBuzz/app/src/androidTest/java/com/example/fizzbuzz/part1fizzbuzz.kt
+++ Work/Training/FizzBuzz/app/src/androidTest/java/com/example/fizzbuzz/part1fizzbuzz.kt
@@ -2,20 +2,48 @@ package com.example.fizzbuzz
 
 fun main() {
     var x = 1
-    while (x <= 100) {
-        if (x % 3 == 0 || x % 5 == 0) {
+
+    while (x <= 300) {
+        var str = ""
+        if (x % 3 == 0 || x % 5 == 0 || x % 7 == 0 || x % 11 == 0 || x % 13 == 0) {
+            if (x % 11 == 0) {
+                if(x % 13 == 0) {
+                    str += "Fezz"
+                }
+                str += "Bong"
+                x += 1
+                println(str)
+                continue
+            }
             if (x % 3 == 0) {
-                print("Fizz")
+                str += "Fizz"
+            }
+            if (x % 13 == 0) {
+                str += "Fezz"
             }
             if (x % 5 == 0) {
-                print("Buzz")
+                str += "Buzz"
+            }
+            if (x % 7 == 0) {
+                str += "Bang"
+            }
+            if (x % 17 == 0) {
+                str = rev(str)
             }
         }
         else {
-            print(x)
+            str += x
         }
         x += 1
-        println()
+        println(str)
     }
-
+}
+fun rev(a: String): String {
+    var cp = a
+    var str = ""
+    while (cp != "") {
+        str += cp.substring(cp.length - 4, cp.length)
+        cp = cp.substring(0, cp.length - 4)
+    }
+    return str
 }
