7ef71c90f672e2cf905c0cd8b060349bf447aeb1
Tasneem Kurabadwala <Tasneem.Kurabadwala@softwire.com>
2024-06-25 15:12:16 +0100

    Made suggested changes

--- Work/Training/FizzBuzz/app/src/androidTest/java/com/example/fizzbuzz/part1fizzbuzz.kt
+++ Work/Training/FizzBuzz/app/src/androidTest/java/com/example/fizzbuzz/part1fizzbuzz.kt
@@ -1,49 +1,52 @@
 package com.example.fizzbuzz
 
 fun main() {
-    var x = 1
+    var loopNumber = 1
 
-    while (x <= 300) {
-        var str = ""
-        if (x % 3 == 0 || x % 5 == 0 || x % 7 == 0 || x % 11 == 0 || x % 13 == 0) {
-            if (x % 11 == 0) {
-                if(x % 13 == 0) {
-                    str += "Fezz"
-                }
-                str += "Bong"
-                x += 1
-                println(str)
-                continue
-            }
-            if (x % 3 == 0) {
-                str += "Fizz"
-            }
-            if (x % 13 == 0) {
-                str += "Fezz"
-            }
-            if (x % 5 == 0) {
-                str += "Buzz"
-            }
-            if (x % 7 == 0) {
-                str += "Bang"
-            }
-            if (x % 17 == 0) {
-                str = rev(str)
-            }
-        }
-        else {
-            str += x
-        }
-        x += 1
-        println(str)
+    while (loopNumber <= 300) {
+        val finalString = calculateFizzBuzz(loopNumber)
+        loopNumber += 1
+        println(finalString)
     }
 }
-fun rev(a: String): String {
-    var cp = a
-    var str = ""
-    while (cp != "") {
-        str += cp.substring(cp.length - 4, cp.length)
-        cp = cp.substring(0, cp.length - 4)
+
+fun calculateFizzBuzz(input: Int): String {
+    var number = input
+    val brokenUpString = mutableListOf<String>()
+
+    if (number % 11 == 0) {
+        if(number % 13 == 0) {
+            brokenUpString.add("Fezz")
+        }
+        brokenUpString.add("Bong")
+        number += 1
+        return convertToString(brokenUpString)
     }
-    return str
+    if (number % 3 == 0) {
+        brokenUpString.add("Fizz")
+    }
+    if (number % 13 == 0) {
+        brokenUpString.add("Fezz")
+    }
+    if (number % 5 == 0) {
+        brokenUpString.add("Buzz")
+    }
+    if (number % 7 == 0) {
+        brokenUpString.add("Bang")
+    }
+    if (number % 17 == 0) {
+        brokenUpString.reverse()
+    }
+    if (brokenUpString.toString() == "[]") {
+        brokenUpString.add(number.toString())
+    }
+    return convertToString(brokenUpString)
+}
+
+fun convertToString(list: MutableList<String>): String {
+    var convertedToString = ""
+    for (item in list) {
+        convertedToString += item
+    }
+    return convertedToString
 }
